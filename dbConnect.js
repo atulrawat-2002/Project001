59c124637471bd09e4c72b838758f248f1286755
Atul <dr6217811@gmail.com>
2025-08-19 10:46:23 +0530

    creating new branch

--- dbConnect.js
+++ dbConnect.js
@@ -3,10 +3,9 @@ const mongoose = require("mongoose");
 
 // Function for database connectivity if unable to connect log the error and exit the process
 module.exports = async() => {
-    const mongoURI = 'mongodb+srv://Initiation:oIbJe1jkQCYrsGIa@namastenodejs.ecrw1k1.mongodb.net/Social-media';
 
     try {
-        const connect = await mongoose.connect(mongoURI)
+        const connect = await mongoose.connect(process.env.MONGO_URI)
         console.log(`Conneted to database ${connect.connection.host}`);
         
     } catch (error) {
