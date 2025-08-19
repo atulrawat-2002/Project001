909851448c64a457b1d6432f8204c7f0b1dba8a0
Atul <dr6217811@gmail.com>
2025-08-19 17:33:58 +0530

    "fixing"

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json({limit: "5mb"}));
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:5173"
+    origin: "https://project001-frontend.onrender.com"
 }))
 
 
