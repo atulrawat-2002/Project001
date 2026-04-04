06ee8524f3355f408c17426172f0d3ae309e4c80
Atul <dr6217811@gmail.com>
2026-04-04 13:07:14 +0530

    Ping youtube and slack removed

--- index.js
+++ index.js
@@ -52,16 +52,16 @@ async function pingSlack_backend() {
 }
 
 
-    setInterval(async () => {
-        try {
-            console.log("sending requests to youtube's backend\n")
-            await pingYoutubeWatchParty();
-            console.log("sending requests to slack's backend\n")
-            await pingSlack_backend();
-        } catch (error) {
-            console.log('Error in request interval', error.message);
-        }
-    }, 1000 * 60 * 10);
+    // setInterval(async () => {
+    //     try {
+    //         console.log("sending requests to youtube's backend\n")
+    //         await pingYoutubeWatchParty();
+    //         console.log("sending requests to slack's backend\n")
+    //         await pingSlack_backend();
+    //     } catch (error) {
+    //         console.log('Error in request interval', error.message);
+    //     }
+    // }, 1000 * 60 * 10);
 
 
 
